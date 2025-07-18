ca64989c9d1ad835f4fa2e06a01882294413d379
ZharasAT <zharas.tuyanov@gmail.com>
2025-07-18 15:04:03 +0500

    Implemented Profile Editing: view, forms, edit_profile.html

--- friends/profiles/urls.py
+++ friends/profiles/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import profile_view
+from .views import profile_view, edit_profile
 
 app_name = "profiles"
 
 urlpatterns = [
+    path("edit/", edit_profile, name="edit_profile"),
     path("<str:username>/", profile_view, name="profile"),
 ]
--- friends/profiles/views.py
+++ friends/profiles/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
 
+from .forms import UserProfileForm
 from .models import UserProfile
 
 User = get_user_model()
@@ -12,3 +13,17 @@ def profile_view(request, username):
     user = get_object_or_404(User, username=username)
     profile = get_object_or_404(UserProfile, user=user)
     return render(request, "profiles/profile.html", {"profile": profile})
+
+@login_required
+def edit_profile(request):
+    profile = request.user.userprofile
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profiles:profile", username=request.user.username)
+    else:
+        form = UserProfileForm(instance=profile)
+
+    return render(request, "profiles/edit_profile.html", {"form": form})
