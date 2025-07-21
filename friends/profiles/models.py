d6669a814890e9e9c48df3f8f839ec932a5a57a7
Aisha <aisha.zhumagul@alumni.nu.edu.kz>
2025-07-21 16:03:14 +0500

    fix auth issue

--- friends/profiles/models.py
+++ friends/profiles/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import CustomUser
+
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
--- friends/profiles/views.py
+++ friends/profiles/views.py
@@ -1,10 +1,9 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
-
-from .forms import UserProfileForm
-from .models import UserProfile
-from .posts.models import Post
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from posts.models import Post
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 User = get_user_model()
 
@@ -20,6 +19,7 @@ def profile_view(request, username):
         "posts": posts,
         "is_own_profile": is_own_profile
     })
+
 
 @login_required
 def edit_profile(request):
