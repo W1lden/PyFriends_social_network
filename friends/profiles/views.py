9ba2c20ce1caf5f867f9d41de3063505b293fdf6
ZharasAT <zharas.tuyanov@gmail.com>
2025-07-23 11:47:51 +0500

    Checking by isort & flake8

--- friends/profiles/views.py
+++ friends/profiles/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponseForbidden
-
+from django.shortcuts import get_object_or_404, redirect, render
 from posts.models import Post
 from profiles.forms import UserProfileForm
 from profiles.models import UserProfile
